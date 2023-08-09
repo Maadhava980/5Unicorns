3ecf79344b422b7329c0b33ec49a07decbd5220a
reiica05 <S10241811@connect.np.edu.sg>
2023-08-09 11:43:14 +0800

    cash on hand part 4/5

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -56,3 +56,51 @@ def cash_on_hand_function():
 
         # if decreasing everyday, goes to the next function to find largest deficit and day
         elif decreasing:
+  #--------------- PART 4: COH decrease function --------------#
+            def find_largest_deficit(data):
+                max_deficit_day, max_deficit_amount = 0, 0
+                prev_cash_on_hand = data[0][1] # Set the initial previous cash on hand
+                for day, cash_on_hand in data[1:]:
+                    deficit = prev_cash_on_hand - cash_on_hand
+                    if deficit > max_deficit_amount:
+                        max_deficit_day, max_deficit_amount = day, deficit
+                    prev_cash_on_hand = cash_on_hand
+                return max_deficit_day, max_deficit_amount
+
+            max_deficit_day, max_deficit_amount = find_largest_deficit(cashOnHandRecords)
+            # Print the highest increase in Cash-on-Hand in the desired format.
+            result1 += ("\n[CASH DEFICIT] CASH ON EACH DAY IS LOWER THAN THE PREVIOUS DAY")
+            result1 += (f"[HIGHEST CASH DEFICIT] DAY: {max_deficit_day}, AMOUNT: USD {max_deficit_amount:.2f}\n")
+            with open ("Summary_report.txt","a") as file:
+                file.write(result1)
+
+        # fluctuate everyday, goes to this function that finds the deficit for each day there ere differences
+        else:
+            #--------------- PART 5: COH fluctuate function --------------#
+            def compute_cash_on_hand_difference(data):
+                differences = []
+                prev_cash_on_hand = data[0][1] # Set the initial previous cash on hand
+                for day, cash_on_hand in data[1:]:
+                    if cash_on_hand < prev_cash_on_hand:
+                        difference = cash_on_hand - prev_cash_on_hand
+                        differences.append((day, difference * -1))
+                    prev_cash_on_hand = cash_on_hand
+                return differences
+
+            # Calculate the differences in Cash-on-Hand only if it's lower than the previous day.
+            differences = compute_cash_on_hand_difference(cashOnHandRecords)
+            for day, amount in differences:
+                result1 += (f"[ CASH DEFICIT ] DAY: {day:3}, AMOUNT: USD {amount:.2f}\n")
+            with open ("Summary_report.txt","a") as file:
+                file.write(result1)
+
+            return result1 
+        return result1
+    COH_trend_result = check_COH_trend(cashOnHandRecords)
+    return COH_trend_result
+
+#call the Cash_on_hand function and capture its result
+cash_on_hand_result = cash_on_hand_function()
+
+#print the result
+print(cash_on_hand_result)
