269c846873edc376c4d4735f1af835711275491d
Maadhava980 <ch.maadhavasai@gmail.com>
2023-08-10 14:41:03 +0800

    docstrings

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import overheads, cash_on_hand, profit_loss
-
+# modularization done to call upon main functions from respective modules
 def main():
     overhead_result = overheads.overhead_func()
     cash_result = cash_on_hand.cash_on_hand_function()
     profit_result = profit_loss.profit_loss_function()
 
-    return overhead_result, cash_result, profit_result
+    return overhead_result, cash_result, profit_result 
